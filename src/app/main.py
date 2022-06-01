50b9406f25d204749b008f61fcf123f693e05fe2
Gracechung-sw <hjngy0511@gmail.com>
2022-06-02 04:00:50 +0900

    Add main script to run this app

---
+++ src/app/main.py
@@ -0,0 +1,34 @@
+import datetime
+
+from adapters.gcal_client import GoogleCalendarClient
+from adapters.slack_client import SlackClient
+
+STATUS = {
+    "연차": {'status_text': '[연차] Paid Time Off', 'status_emoji': ':shushing_face:'},
+    "보상휴가": {'status_text': '[보상휴가] Paid Time Off', 'status_emoji': ':shushing_face:'},
+    "재택근무": {'status_text': '[재택] Work From Home', 'status_emoji': ':house:'},
+}
+CALENDAR_NAME = 'hjngy0511@gmail.com'
+
+def main():
+    try:
+        calendar = GoogleCalendarClient()
+        messaging = SlackClient()
+
+        start_date = datetime.date.today().strftime("%Y-%m-%d") + 'T00:00:00Z' # 'Z' indicates UTC time
+        end_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d") + 'T00:00:00Z' # 'Z' indicates UTC time
+
+        events = calendar.get_events(CALENDAR_NAME, start_date, end_date)
+        print("events", events)
+
+        for event in events:
+            user_name, event_name = event["user_name"], event["event_summary"]
+            if event_name in STATUS:
+                print(user_name, event_name) # ex. 정현정 연차
+                messaging.update_status(user_name, STATUS[event_name])
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
